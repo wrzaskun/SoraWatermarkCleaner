c9e8a995cb9b7cec35b90be9ccac463844e3b386
wrzaskun <wrzaskun000@gmail.com>
2025-12-15 02:57:42 +0100

    enum fix

--- sorawm/schemas.py
+++ sorawm/schemas.py
@@ -1,6 +1,7 @@
-from enum import StrEnum
+from enum import Enum
 
 
-class CleanerType(StrEnum):
+class CleanerType(str, Enum):
     LAMA = "lama"
+    E2FGVI = "e2fgvi"
     E2FGVI_HQ = "e2fgvi_hq"
