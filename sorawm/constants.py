42003b24a232629082be69f3bd48832c277c144e
wrzaskun <wrzaskun000@gmail.com>
2025-12-15 02:56:41 +0100

    fix chunk size to avoid OOM

--- sorawm/constants.py
+++ sorawm/constants.py
@@ -1,1 +1,11 @@
-CHUNK_SIZE_PER_GB_VRAM = 5  # 1GB can process about 5 frames in chunk size
+'''
+Memory requirements for sorawm.constants:
+
+- 1GB of VRAM is required per chunk when processing HD 1080p video.
+- Processing a full HD 1080p movie typically requires around 16-18GB of VRAM for 1 chunk.
+- Using 2 chunks per GB increases VRAM usage to approximately 22-26GB.
+- Using 5 chunks per GB increases VRAM usage to approximately 26-36GB.
+
+'''
+CHUNK_SIZE_PER_GB_VRAM = 1 
+
